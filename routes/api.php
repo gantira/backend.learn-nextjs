051cd08788a946a370a2c6a074f3d4ce940f3545
Angga Gantira <angga.gantira@gmail.com>
2021-01-27 09:08:34 +0700

    8 Mari Kita Persiapkan Datanya

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('posts')->group(function () {
+    Route::get('', [PostController::class, 'index']);
+    Route::get('{post:slug}', [PostController::class, 'show']);
 });
